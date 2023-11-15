f0e231e2002aad9da2a8f2505ef420196e639082
valeriayunga <vayunga@utpl.edu.ec>
2023-11-15 12:20:12 -0500

    InicioCuso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import {RegisterComponent} from "./components/register/register.component";
+import {InicioCursoComponent} from "./components/inicio-curso/inicio-curso.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'inicio', pathMatch:'full'},
   {path: 'login', component: LoginComponent},
   {path: 'inicio', component: InicioComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'inicioCurso', component: InicioCursoComponent},
   {path: '**', redirectTo: 'inicio', pathMatch:'full' }
 ];
 
