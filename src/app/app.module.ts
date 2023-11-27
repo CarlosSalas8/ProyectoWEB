7a3a92cbe8edb9da75d1d06c8396e605c76be8d7
valeriayunga <92813866+valeriayunga@users.noreply.github.com>
2023-11-27 08:29:11 -0500

    comitSinErrores
    Sin Errores de Carlos >:c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
+
 import { ReactiveFormsModule } from '@angular/forms';
-=======
 import {AuthService} from "./services/login.service";
->>>>>>> ccc97fa027546750d111eab14aae861467f96f5b
 
 //Components
 import { InicioComponent } from './components/inicio/inicio.component';
@@ -19,13 +16,10 @@ import {HeaderComponent} from "./components/header/header.component";
 import {RegisterComponent} from "./components/register/register.component";
 import { InicioCursoComponent } from './components/inicio-curso/inicio-curso.component';
 import { AprenderContabilidadComponent } from './components/aprender-contabilidad/aprender-contabilidad.component';
-<<<<<<< HEAD
 import { EstadoComponent } from './components/estado/estado.component';
-=======
 import { FooterComponent } from './footer/footer.component';
 import { ContenidoCursoComponent } from './contenido-curso/contenido-curso.component';
 
->>>>>>> ccc97fa027546750d111eab14aae861467f96f5b
 
 @NgModule({
   declarations: [
@@ -36,22 +30,16 @@ import { ContenidoCursoComponent } from './contenido-curso/contenido-curso.compo
     HeaderComponent,
     InicioCursoComponent,
     AprenderContabilidadComponent,
-<<<<<<< HEAD
-    EstadoComponent
-=======
+    EstadoComponent,
     FooterComponent,
     ContenidoCursoComponent
->>>>>>> ccc97fa027546750d111eab14aae861467f96f5b
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     FormsModule,
-    ReactiveFormsModule
-=======
+    ReactiveFormsModule,
     FormsModule
->>>>>>> ccc97fa027546750d111eab14aae861467f96f5b
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
