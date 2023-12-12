5eb4be369e07059edf9f9774b80cf20350cef107
CarlosSalas8 <92597874+CarlosSalas8@users.noreply.github.com>
2023-12-12 10:13:52 -0500

    Carlos
    Cambios estado

--- src/app/components/estado/estado.component.ts
+++ src/app/components/estado/estado.component.ts
@@ -1,48 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-estado',
   templateUrl: './estado.component.html',
   styleUrls: ['./estado.component.css']
 })
-
 export class EstadoComponent implements OnInit {
-  miFormulario!: FormGroup; // Agregamos '!' al final
+  miFormulario!: FormGroup;
+  ingresos: number = 0;
+  gastos: number = 0;
+  proyecciones: number = 0;
+  beneficios: number = 0;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.miFormulario = this.fb.group({
-      inversionInicial: ['', Validators.required],
-      inversionProgresiva: ['', Validators.required],
-      presupuesto: ['', Validators.required],
-      ganancias: [''],
-      perdidas: ['']
+      precioVentaPorUnidad: ['', Validators.required],
+      cantidadProyectada: ['', Validators.required],
+      ingresosAdicionales: [''],
+      costoPorUnidad: ['', Validators.required],
+      gastosOperativos: ['', Validators.required],
+      gastosMarketing: ['', Validators.required],
+      gastosDesarrollo: ['', Validators.required],
+      gastosAdicionales: [''],
+      ingresos: [{ value: 0, disabled: true }],
+      gastos: [{ value: 0, disabled: true }],
+      proyecciones: [{ value: 0, disabled: true }],
+      beneficios: [{ value: 0, disabled: true }],
     });
-    this.miFormulario.get('perdidas')?.disable();
-    this.miFormulario.get('ganancias')?.disable()
   }
 
-  // Función para actualizar automáticamente ganancias y pérdidas
+  // Función para actualizar automáticamente ingresos, gastos, proyecciones, beneficios
   actualizarEstado(): void {
-    const inversionInicial = this.miFormulario.value.inversionInicial;
-    const inversionProgresiva = this.miFormulario.value.inversionProgresiva;
-    const presupuesto = this.miFormulario.value.presupuesto;
+    const emprendimiento = this.miFormulario.value;
 
-    const inversionTotal = inversionInicial + inversionProgresiva;
+    const calcularTotalIngresos = () => {
+      return (
+        (emprendimiento.ingresosAdicionales || 0) +
+        emprendimiento.precioVentaPorUnidad * emprendimiento.cantidadProyectada
+      );
+    };
+    
+    const calcularTotalGastos = () => {
+      return (
+        (emprendimiento.costoPorUnidad * emprendimiento.cantidadProyectada) +
+        emprendimiento.gastosOperativos +
+        emprendimiento.gastosMarketing +
+        emprendimiento.gastosDesarrollo +
+        (emprendimiento.gastosAdicionales || 0)
+      );
+    };
 
-    if (inversionTotal <= presupuesto) {
-      this.miFormulario.patchValue({
-        ganancias: presupuesto - inversionTotal,
-        perdidas: 0
-      });
-    } else {
-      this.miFormulario.patchValue({
-        ganancias: 0,
-        perdidas: inversionTotal - presupuesto
-      });
-    }
+    
+    
+    const calcularBeneficios = () => {
+      return calcularTotalIngresos() - calcularTotalGastos();
+    };
+
+    this.miFormulario.get('ingresos')?.setValue(calcularTotalIngresos());
+    this.miFormulario.get('gastos')?.setValue(calcularTotalGastos());
+    this.miFormulario.get('beneficios')?.setValue(calcularBeneficios());
   }
 }
