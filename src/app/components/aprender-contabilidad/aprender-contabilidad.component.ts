bb4f79ecb58f08566f1155203a7c64983bf3b6c4
valeriayunga <92813866+valeriayunga@users.noreply.github.com>
2023-12-27 22:04:57 -0500

    agragar ver mas en aprenderContabilidad

--- src/app/components/aprender-contabilidad/aprender-contabilidad.component.ts
+++ src/app/components/aprender-contabilidad/aprender-contabilidad.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {AuthService} from "../../services/login.service";
+import { AuthService } from '../../services/login.service';
 
 @Component({
   selector: 'app-aprender-contabilidad',
@@ -7,14 +7,32 @@ import {AuthService} from "../../services/login.service";
   styleUrls: ['./aprender-contabilidad.component.css']
 })
 export class AprenderContabilidadComponent {
-  cursos: any[] | undefined;
+  cursos: any[] = [];
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.authService.getTodosLosCursos().subscribe(
-      data => this.cursos = data,
+      data => {
+        this.cursos = data || [];
+        this.cursos.forEach(curso => (curso.showMore = false));
+      },
       err => console.error(err),
     );
+  }
+
+  toggleDescription(curso: any): void {
+    curso.showMore = !curso.showMore;
+  }
+
+  truncateDescription(description: string, wordCount: number, showMore: boolean): string {
+    if (showMore) {
+      return description; // Mostrar el texto completo si 'showMore' es verdadero
+    } else {
+      const words = description.split(' ');
+      const truncated = words.slice(0, wordCount).join(' ');
+
+      return truncated + (words.length > wordCount ? '...' : '');
+    }
   }
 }
