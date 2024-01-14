c8f12ffded98334ef2007dd0d68b726c57b9326e
valeriayunga <92813866+valeriayunga@users.noreply.github.com>
2024-01-13 22:50:23 -0500

    nuevo frontend de los cursos

--- src/app/components/contenido-curso/contenido-curso.component.ts
+++ src/app/components/contenido-curso/contenido-curso.component.ts
@@ -1,48 +1,62 @@
-import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import {Component, OnInit, AfterViewInit, ElementRef} from '@angular/core';
+import { Stepper, Carousel,initTE,} from 'tw-elements';
 import { ActivatedRoute } from '@angular/router';
-import { AuthService } from "../../services/login.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {map, Observable, switchMap} from "rxjs";
+import {AuthService} from "../../services/login.service";
 
 @Component({
   selector: 'app-contenido-curso',
   templateUrl: './contenido-curso.component.html',
   styleUrls: ['./contenido-curso.component.css']
+
 })
-export class ContenidoCursoComponent implements OnInit, AfterViewInit {
-  openStepIndex: number | null = 0; // Iniciar con el primer paso abierto
+export class ContenidoCursoComponent implements OnInit,AfterViewInit{
+  openStepIndex: number | null = null;
+  id: number | undefined;
+  slides = []; // <-- Agregue esta línea
   contenidoCurso: Array<any> = [];
-  activeSlideIndex = 0;
-
-  constructor(
-    private route: ActivatedRoute,
-    private authService: AuthService,
-    private elRef: ElementRef
-  ) {}
-
+  constructor(private route: ActivatedRoute,
+              private http: HttpClient,
+              private authService: AuthService,
+              private elRef: ElementRef) {}
   ngOnInit() {
     const idCurso = this.route.snapshot.params['id'];
     this.authService.getContenidoCurso(idCurso)
       .then(contenidoCurso => {
         this.contenidoCurso = contenidoCurso;
-        // Inicializar el carrusel con los pasos del curso
-        // Si estás usando un carrusel o cualquier otro componente de tw-elements, asegúrate de inicializarlo aquí si es necesario.
+        (this.slides as number[]) = this.contenidoCurso.map((_x, i) => i); // <-- Además de esta línea
       })
       .catch(error => console.error(error));
+      this.openStepIndex = 0;
+
   }
+
 
   ngAfterViewInit(): void {
-    // Si tw-elements requiere inicialización, hazlo aquí.
-    // initTE(); // Descomenta esta línea si necesitas inicializar tw-elements manualmente.
   }
+  activeSlideIndex = 0;
 
   nextSlide() {
-    this.activeSlideIndex = (this.activeSlideIndex + 1) % this.contenidoCurso.length;
+    const nextIndex = (this.activeSlideIndex + 1) % this.contenidoCurso.length;
+
+    this.activeSlideIndex = nextIndex;
   }
 
+  // Actualiza la función prevSlide para abrir el paso anterior y cerrar los demás
   prevSlide() {
-    this.activeSlideIndex = (this.activeSlideIndex - 1 + this.contenidoCurso.length) % this.contenidoCurso.length;
+    const prevIndex = (this.activeSlideIndex - 1 + this.contenidoCurso.length) % this.contenidoCurso.length;
+
+    this.activeSlideIndex = prevIndex;
+  }
+  toggleStep(index: number) {
+    if (this.openStepIndex === index) {
+      // Si el paso ya está abierto, lo cerramos
+      this.openStepIndex = null;
+    } else {
+      // Abrimos el paso clickeado y cerramos los otros
+      this.openStepIndex = index;
+    }
   }
 
-  toggleStep(index: number) {
-    this.openStepIndex = this.openStepIndex === index ? null : index;
-  }
 }
